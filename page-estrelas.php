121f21387985b39e79366aadd0cab5b42a31c4af
iagobalmeida <iago.borba.almeida@gmail.com>
2022-03-14 11:36:13 -0300

    - O texto dos streamers/staff está sem quebra de linha, está gerando uma barra de rolagem dentro da descrição - Adicionar link TWITCH nos streamers; - O nome dos stremears está ficando todo em letra minúscula depois de cadastrados; - Tem alguma forma de organizar os streamears na página? Ou a ordem é sempre a de cadastro? - Altera o tamanho mínimo das imagens para 1000x1000

--- page-estrelas.php
+++ page-estrelas.php
@@ -1,10 +1,12 @@
 <?php
-$args = array(
-    'numberposts' => 10,
-    'post_type'   => 'streamer'
-);
+$streamerList = new WP_Query(array(
+    'meta_key'          => 'ordem',
+    'order'             => 'ASC',
+    'orderby'           => 'meta_value',
+    'post_type'         => 'streamer',
+    'posts_per_page'    => -1,
+));
 
-$streamers = get_posts($args);
 get_header();
 ?>
 <style>
@@ -23,62 +25,63 @@ get_header();
     </div>
 </div>
 <div class="row mb-0">
-    <?php foreach($streamers as $streamer): ?>
-    <?php $customFields = get_fields($streamer->ID); ?>
-    <div class="col-card mb-60">
-        <div class="img-wrapper">
-            <div class="img-text">
+    <?php if($streamerList->have_posts()): while($streamerList->have_posts()): $streamerList->the_post(); ?>
+        <div class="col-card mb-60">
+            <div class="img-wrapper">
+                <div class="img-text">
+                    <div class="text">
+                        <?=the_content()?>
+                    </div>
+                    <div class="social-icons">
+                        <?php 
+                            $url_twitch     = get_field('url_twitch') ? get_field('url_twitch') : '#';
+                            $url_discord    = get_field('url_discord') ? get_field('url_discord') : '#';
+                            $url_twitter    = get_field('url_twitter') ? get_field('url_twitter') : '#';
+                            $url_instagram  = get_field('url_instagram') ? get_field('url_instagram') : '#';
+                            $url_youtube    = get_field('url_youtube') ? get_field('url_youtube') : '#';
+                            $url_facebook   = get_field('url_facebook') ? get_field('url_facebook') : '#';
+                            $url_tiktok     = get_field('url_tiktok') ? get_field('url_tiktok') : '#';
+                        ?>
+                        <a href="<?= $url_twitch ?>" class="<?= $url_twitch == '#' ? 'disabled' : '' ?>">
+                            <?= file_get_contents(get_template_directory_uri().'/assets/icons/discord-b.svg'); ?>
+                        </a>
+                        <a href="<?= $url_discord ?>" class="<?= $url_discord == '#' ? 'disabled' : '' ?>">
+                            <?= file_get_contents(get_template_directory_uri().'/assets/icons/discord-b.svg'); ?>
+                        </a>
+                        <a href="<?= $url_twitter ?>" class="<?= $url_twitter == '#' ? 'disabled' : '' ?>">
+                            <?= file_get_contents(get_template_directory_uri().'/assets/icons/twitter-b.svg'); ?>
+                        </a>
+                        <a href="<?= $url_instagram ?>" class="<?= $url_instagram == '#' ? 'disabled' : '' ?>">
+                            <?= file_get_contents(get_template_directory_uri().'/assets/icons/insta-b.svg'); ?>
+                        </a>
+                        <a href="<?= $url_youtube ?>" class="<?= $url_youtube == '#' ? 'disabled' : '' ?>">
+                            <?= file_get_contents(get_template_directory_uri().'/assets/icons/yt-b.svg'); ?>
+                        </a>
+                        <a href="<?= $url_facebook ?>" class="<?= $url_facebook == '#' ? 'disabled' : '' ?>">
+                            <?= file_get_contents(get_template_directory_uri().'/assets/icons/face-b.svg'); ?>
+                        </a>
+                        <a href="<?= $url_tiktok ?>" class="<?= $url_tiktok == '#' ? 'disabled' : '' ?>">
+                            <?= file_get_contents(get_template_directory_uri().'/assets/icons/tiktok-b.svg'); ?>
+                        </a>
+                    </div>
+                </div>
+                <img src="<?= get_field('imagem') ?>">
+            </div>
+            <div class="title-wrapper">
                 <div class="text">
-                    <?=$streamer->post_content;?>
+                    <h3 class="title"><?=the_title()?></h3>
+                    <h5 class="subtitle">
+                        <?=get_field('subtitulo')?>
+                    </h5>
                 </div>
-                <div class="social-icons">
-                    <?php 
-                        $url_discord    = isset($customFields['url_discord']) && $customFields['url_discord'] ? $customFields['url_discord'] : '#';
-                        $url_twitter    = isset($customFields['url_twitter']) && $customFields['url_twitter'] ? $customFields['url_twitter'] : '#';
-                        $url_instagram  = isset($customFields['url_instagram']) && $customFields['url_instagram'] ? $customFields['url_instagram'] : '#';
-                        $url_youtube    = isset($customFields['url_youtube']) && $customFields['url_youtube'] ? $customFields['url_youtube'] : '#';
-                        $url_facebook   = isset($customFields['url_facebook']) && $customFields['url_facebook'] ? $customFields['url_facebook'] : '#';
-                        $url_link       = isset($customFields['url_link']) && $customFields['url_link'] ? $customFields['url_link'] : '#';
-                    ?>
-                    <a href="<?= $url_discord ?>" class="<?= $url_discord == '#' ? 'disabled' : '' ?>">
-                        <?= file_get_contents(get_template_directory_uri().'/assets/icons/discord-b.svg'); ?>
-                    </a>
-                    <a href="<?= $url_twitter ?>" class="<?= $url_twitter == '#' ? 'disabled' : '' ?>">
-                        <?= file_get_contents(get_template_directory_uri().'/assets/icons/twitter-b.svg'); ?>
-                    </a>
-                    <a href="<?= $url_instagram ?>" class="<?= $url_instagram == '#' ? 'disabled' : '' ?>">
-                        <?= file_get_contents(get_template_directory_uri().'/assets/icons/insta-b.svg'); ?>
-                    </a>
-                    <a href="<?= $url_youtube ?>" class="<?= $url_youtube == '#' ? 'disabled' : '' ?>">
-                        <?= file_get_contents(get_template_directory_uri().'/assets/icons/yt-b.svg'); ?>
-                    </a>
-                    <a href="<?= $url_facebook ?>" class="<?= $url_facebook == '#' ? 'disabled' : '' ?>">
-                        <?= file_get_contents(get_template_directory_uri().'/assets/icons/face-b.svg'); ?>
-                    </a>
-                    <a href="<?= $url_link ?>" class="<?= $url_link == '#' ? 'disabled' : '' ?>">
-                        <?= file_get_contents(get_template_directory_uri().'/assets/icons/link-b.svg'); ?>
-                    </a>
-                </div>
+                <span class="svg-plus"><?= file_get_contents(get_template_directory_uri().'/assets/icons/plus-w.svg'); ?></span>
             </div>
-            <img src="<?= $customFields['imagem']; ?>">
         </div>
-        <div class="title-wrapper">
-            <div class="text">
-                <h3 class="title"><?=$streamer->post_name?></h3>
-                <h5 class="subtitle">
-                    <?=$customFields['subtitulo']?>
-                </h5>
-            </div>
-            <span class="svg-plus"><?= file_get_contents(get_template_directory_uri().'/assets/icons/plus-w.svg'); ?></span>
-        </div>
-    </div>
-    <?php endforeach; ?>
-</div>
-<pre>
-<?php
+    <?php endwhile; else: ?>
 
-?>
-</pre>
+    <?php endif; ?>
+
+</div>
 
 <?php
 get_footer();
